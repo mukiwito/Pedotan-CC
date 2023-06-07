281db5a6628e29cabdb1d12535765b05f3a6f910
Goldragon <16520417@std.stei.itb.ac.id>
2023-06-07 21:20:57 +0700

    input data

--- auth/app.py
+++ auth/app.py
@@ -63,10 +63,41 @@ class LogoutResource(Resource):
             return {'message': 'Invalid session token.'}, 401
         except auth.EmailNotFoundError:
             return {'message': 'Email not found.'}, 401
+        
+class InputDataUserResource(Resource):
+    def post(self):
+        name = request.json.get('name')
+        email = request.json.get('email')
+        noHandphone = request.json.get('noHandphone')
+        nik = request.json.get('nik')
+        photo = request.json.get('photo')
+        location = request.json.get('location')
+
+        try:
+
+            user = auth.get_user_by_email(email)
+            
+            user_data = {
+                'name': name,
+                'email': email,
+                'noHandphone': noHandphone,
+                'nik': nik,
+                'photo': photo,
+                'location': location
+            }
+
+            db = firestore.client()
+            db.collection('email').document(user.uid).set(user_data)
+
+            return {'message': 'User Data Has Been Saved'}, 201
+        except auth.InvalidEmailError:
+            return {'message': 'Use A Valid Email Address'}, 401
+        
 
 api.add_resource(RegisterResource, '/auth/register')
 api.add_resource(AuthTokenResource, '/auth/token')
 api.add_resource(LogoutResource, '/auth/logout')
+api.add_resource(InputDataUserResource, '/auth/data')
 
 if __name__ == '__main__':
     app.run(debug=True)
