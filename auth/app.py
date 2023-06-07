c649dbfe34dcf77886ba53556d7185a60b2d5fee
Goldragon <16520417@std.stei.itb.ac.id>
2023-06-07 13:17:46 +0700

    logout

--- auth/app.py
+++ auth/app.py
@@ -17,12 +17,12 @@ class RegisterResource(Resource):
         try:
             # Register User in Firebase
             user = auth.create_user(
-                email = email,
-                password = password
+                email=email,
+                password=password
             )
-            return {'message' : 'User Created Successfully'}, 201
+            return {'message': 'User Created Successfully'}, 201
         except auth.EmailAlreadyExistsError:
-            return {'message' : 'Email Already Exists'}, 409
+            return {'message': 'Email Already Exists'}, 409
 
 class AuthTokenResource(Resource):
     # GET CUSTOM SESSION TOKEN per user
@@ -34,19 +34,31 @@ class AuthTokenResource(Resource):
             user = auth.get_user_by_email(email)
             # Session Token
             session_claims = {
-                'uid' : user.uid,
-                'custom_claim' : 'AIzaSyBzQInEHejnK3ogHBLSGi9bC_SKiEfdr4g' # Web API Key
+                'uid': user.uid,
+                'custom_claim': 'your_web_api_key'  # Replace with your Web API Key
             }
             session_token = auth.create_custom_token(user.uid, session_claims)
 
-            return {'token' : session_token.decode('utf-8')}, 200    
+            return {'token': session_token.decode('utf-8')}, 200
         except auth.InvalidIdTokenError:
             return {'message': 'Invalid credentials.'}, 401
         except auth.EmailNotFoundError:
             return {'message': 'Email not found.'}, 401
 
+class LogoutResource(Resource):
+    def post(self):
+        token = request.json.get('token')
+
+        try:
+            # Revoke the given token
+            auth.revoke_refresh_tokens(token)
+            return {'message': 'User logged out successfully'}, 200
+        except auth.InvalidSessionCookieError:
+            return {'message': 'Invalid session token.'}, 401
+
 api.add_resource(RegisterResource, '/auth/register')
 api.add_resource(AuthTokenResource, '/auth/token')
+api.add_resource(LogoutResource, '/auth/logout')
 
 if __name__ == '__main__':
     app.run(debug=True)
