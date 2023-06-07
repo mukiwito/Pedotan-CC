f67ddfde2e4032d461ba5589b6c0b5e7894aa37d
Dzaki Putranto <muhammaddzakidwiputranto@gmail.com>
2023-06-07 14:26:22 +0700

    add token to database

--- auth/app.py
+++ auth/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 import firebase_admin
-from firebase_admin import auth, credentials
+from firebase_admin import auth, credentials, firestore
 
 app = Flask(__name__)
 api = Api(app)
@@ -39,6 +39,13 @@ class AuthTokenResource(Resource):
             }
             session_token = auth.create_custom_token(user.uid, session_claims)
 
+            session_token_doc = {
+                'uid': user.uid,
+                'token': session_token.decode('utf-8')
+            }
+            db = firestore.client()
+            db.collection('session token').document('session_token').set(session_token_doc)
+
             return {'token': session_token.decode('utf-8')}, 200
         except auth.InvalidIdTokenError:
             return {'message': 'Invalid credentials.'}, 401
@@ -48,10 +55,14 @@ class AuthTokenResource(Resource):
 class LogoutResource(Resource):
     def post(self):
         token = request.json.get('token')
+        email = request.json.get('email')
+
+        user = auth.get_user_by_email(email)
+        
 
         try:
             # Revoke the given token
-            auth.revoke_refresh_tokens(token)
+            auth.revoke_refresh_tokens(user.uid)
             return {'message': 'User logged out successfully'}, 200
         except auth.InvalidSessionCookieError:
             return {'message': 'Invalid session token.'}, 401
